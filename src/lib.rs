c88a70fadaeab11edd76ff8059903fc99f515d95
dundorma <90293691+dundorma@users.noreply.github.com>
2024-12-27 22:34:14 +0700

    feat: add subscribe boiler

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,17 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
 
@@ -13,6 +23,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         App::new()
             // .wrap(middleware::Logger::default())
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,20 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = oxidize::run(listener).expect("failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let server_address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &server_address))
         .send()
         .await
         .expect("failed to execute request");
@@ -15,10 +23,48 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = oxidize::run(listener).expect("failed to bind address");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+#[tokio::test]
+async fn subscribe_valid_form_data() {
+    let server_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=arip&email=my%40email.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &server_address))
+        .header("Content-type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_invalid_form_data() {
+    let server_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=arip", "missing the email"),
+        ("email=my%40email.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", server_address))
+            .header("Content-type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The api did not fail with 400 bad request when the payload was {}",
+            error_message
+        );
+    }
 }
